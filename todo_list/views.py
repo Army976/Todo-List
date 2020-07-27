b8733b9da6622b8c2b859c96a7f7913fa83f4a14
Army <armaghankhoshaeen@gmail.com>
2020-07-27 21:57:45 +0430

    added edit for items

--- todo_list/views.py
+++ todo_list/views.py
@@ -5,7 +5,6 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 
 def home(request):
-
 	if request.method == 'POST':
 		form = ListForm(request.POST or None)
 
@@ -40,3 +39,18 @@ def uncross(request, list_id):
 	item.completed = False
 	item.save()
 	return redirect('home')
+
+def edit(request, list_id):
+	if request.method == 'POST':
+		item = List.objects.get(pk=list_id)
+		form = ListForm(request.POST or None, instance=item)
+
+		if form.is_valid():
+			form.save()
+			all_items = List.objects.all
+			messages.success(request, ('Item Has Been Edited!'))
+			return redirect('home')
+
+	else:
+		item = List.objects.get(pk=list_id)
+		return render(request, 'edit.html', {'item': item})
